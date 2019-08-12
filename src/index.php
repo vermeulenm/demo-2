a4fa980d30ecbeb57691a02b8549e7f3c263457c
Mark Vermeulen <mark.x.vermeulen.-nd@disney.com>
2019-08-12 14:56:20 -0400

    Update 1 to demo-1 project

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 
 echo "Hello, World from Kubernetes! <br>";
 echo "Hello Jenkins X! <br>";
+echo "Hello Update1! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
 ?>
